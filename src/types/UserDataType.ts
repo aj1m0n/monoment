92057dfb5a1ef8e5bc20b48cf2f68919cbe6180a
aj1m0n <surpasstdu@gmail.com>
2019-06-07 21:11:43 +0900

    update

--- src/types/UserDataType.ts
+++ src/types/UserDataType.ts
@@ -3,4 +3,6 @@ import firebase from "firebase/app";
 export default interface UserDataType {
   createdAt?: firebase.firestore.FieldValue;
   updatedAt?: firebase.firestore.FieldValue;
+  name?: string;
+  workspace?: string;
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,23 @@
     <div class="greeting">
       <h1 class="title is-6">Hello, {{displayName}}.</h1>
     </div>
+    <div class="field">
+      <label class="label">Name</label>
+      <div class="control">
+        <textarea class="textarea" placeholder="Enter name" v-model="name"></textarea>
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Preview</label>
+      <div class="control">
+        <div v-html="sanitizedProfile" class="preview"></div>
+      </div>
+    </div>
+    <div class="field">
+      <div class="control">
+        <button class="button is-link" @click="save">Submit</button>
+      </div>
+    </div>
     <button class="button is-primary" @click="signOut">
       <strong>Sign out</strong>
     </button>
@@ -15,7 +32,7 @@ import "firebase/functions";
 import "firebase/storage";
 
 import firebase from "firebase/app";
-import firestore from "firebase/firestore";
+import firestore from "@/firestore";
 import firebaseui from "firebaseui";
 import "firebaseui/dist/firebaseui.css";
 import { Component, Vue } from "vue-property-decorator";
@@ -23,11 +40,41 @@ import UserDataType from "@/types/UserDataType";
 
 @Component({})
 export default class home extends Vue {
-  private profile: string = "";
+  private name: string = "";
   private debouncedSanitize: any = null;
   private sanitizedProfile: string = "";
   private selectedFile: any = null;
   private imageUrl: string = "";
+
+  private created() {
+    firestore
+      .collection("users")
+      .doc(this.$store.getters.user.uid)
+      .get()
+      .then(doc => {
+        const userData = doc.data();
+        if (doc && userData) {
+          this.name = userData.name;
+        }
+      });
+  }
+  private save() {
+    firestore
+      .collection("users")
+      .doc(this.$store.getters.user.uid)
+      .update({
+        name: this.name,
+        updatedAt: firebase.firestore.FieldValue.serverTimestamp()
+      })
+      .then(() => {
+        alert("Document successfully updated!");
+      });
+  }
+  private async updateSanitizedProfile() {
+    const sanitizeFunc = firebase.functions().httpsCallable("sanitize");
+    const response = await sanitizeFunc({ text: this.name });
+    this.sanitizedProfile = response.data.sanitizedText;
+  }
 
   private signOut() {
     this.$store.dispatch("signOut");
